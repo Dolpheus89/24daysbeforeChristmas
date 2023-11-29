2ebde738152d11da920be4322c38e51be54fd4e3
Florian <Florian.schaessens@gmail.com>
2023-11-29 08:23:37 +0100

    add the function select message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// fenetre modal
+
 const modalContainer = document.querySelector(".modal-container");
 const modalTrigger = document.querySelectorAll(".modal-trigger");
 
@@ -7,4 +9,29 @@ modalTrigger.forEach((trigger) =>
 
 function toggleModal() {
   modalContainer.classList.toggle("active");
+}
+
+// select message
+
+const titleElement = document.querySelector(".modal h1");
+const pElement = document.querySelector(".modal p");
+
+const title = ["", "Test1", "Test2"];
+const message = ["", "Message1", "Message2"];
+
+function changeContent(classList) {
+  const caseNumb = classList[1];
+  const index = parseInt(caseNumb.replace("case", ""));
+  titleElement.innerHTML = title[index];
+  pElement.innerHTML = message[index];
+}
+
+for (let i = 1; i <= 24; i++) {
+  let elements = document.getElementsByClassName("case" + i);
+
+  Array.from(elements).forEach(function (element) {
+    element.addEventListener("click", function () {
+      changeContent(element.classList);
+    });
+  });
 }
